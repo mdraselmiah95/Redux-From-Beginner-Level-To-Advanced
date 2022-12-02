40a9e7dc3be53f45165dd449bc07d9c133cee7f1
mdraselmiah95 <russelroy15@gmail.com>
2022-12-03 01:54:46 +0600

    Learning Redex

--- What-problem-we-are-solving-using-redux/raw-state/src/App.js
+++ What-problem-we-are-solving-using-redux/raw-state/src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
-import { useState } from "react";
+import { createContext, useState } from "react";
+import Parents from "./pages/Parents";
+
+const COUNTER_CONTEXT = createContext();
 
 function App() {
   const [count, setCount] = useState(0);
-  return (
-    <div className="">
-      <h2>{count}</h2>
 
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-    </div>
+  const value = { count, setCount };
+  return (
+    <COUNTER_CONTEXT value={value}>
+      <div>
+        <Parents />
+      </div>
+    </COUNTER_CONTEXT>
   );
 }
 
 export default App;
 
-// This is the coolest the new the top news in the world one you are
+// Not to obeay the rule
