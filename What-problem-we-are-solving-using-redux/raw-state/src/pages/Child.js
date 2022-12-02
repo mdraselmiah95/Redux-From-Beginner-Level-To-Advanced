8bed7d786db510c04d3d896c16a81d23072fbd02
mdraselmiah95 <russelroy15@gmail.com>
2022-12-03 01:54:25 +0600

    Learning Redex

---
+++ What-problem-we-are-solving-using-redux/raw-state/src/pages/Child.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Child = () => {
+  return (
+    <div>
+      <h2>This is child.</h2>
+    </div>
+  );
+};
+
+export default Child;
---
+++ What-problem-we-are-solving-using-redux/raw-state/src/pages/Parents.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Parents = () => {
+  return (
+    <div>
+      <h2>This is cool.</h2>
+    </div>
+  );
+};
+
+export default Parents;
+<h2>This is cool.</h2>;
