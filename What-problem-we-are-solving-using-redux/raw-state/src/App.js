3643123db5366442995cc05026dbf8d4bdf05aa7
mdraselmiah95 <russelroy15@gmail.com>
2022-12-03 23:22:52 +0600

    Learning State management

--- What-problem-we-are-solving-using-redux/raw-state/src/App.js
+++ What-problem-we-are-solving-using-redux/raw-state/src/App.js
@@ -1,28 +1,15 @@
 import "./App.css";
-import { createContext, useState } from "react";
-import Parents from "./pages/Parents";
-import ShortForm from "./pages/ShortForm";
-import Counter from "./pages/Counter";
+import { createContext } from "react";
+import LongForm from "./pages/LongForm";
 
 export const COUNTER_CONTEXT = createContext();
 
 function App() {
-  const [count, setCount] = useState(0);
-  const value = { count, setCount };
-
   return (
-    <COUNTER_CONTEXT.Provider value={value}>
-      <div>
-        <Parents />
-        <br /> <br /> <br />
-        <ShortForm />
-        <br /> <br /> <br />
-        <Counter />
-      </div>
-    </COUNTER_CONTEXT.Provider>
+    <>
+      <LongForm />
+    </>
   );
 }
 
 export default App;
-
-// Always the top of the game and also the find the nice and cool nice and cool
