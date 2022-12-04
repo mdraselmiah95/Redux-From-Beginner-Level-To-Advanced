c10144d345e8a053eb7c874b02ad033fd16beca5
mdraselmiah95 <russelroy15@gmail.com>
2022-12-05 02:42:15 +0600

    Learning State management by moon-tech

--- moon-tech/src/pages/TopRated.js
+++ moon-tech/src/pages/TopRated.js
@@ -1,9 +1,39 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 const TopRated = () => {
+  const {
+    state: { products, loading, error },
+  } = useProducts();
+
+  let content;
+
+  if (loading) {
+    content = (
+      <div className="justify-items-center	justify-center">
+        <div className="justify-center animate-spin rounded-full h-32 w-32 border-b-2 border-blue-400"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    content = <p>Something went wrong 💥</p>;
+  }
+
+  if (!loading && !error && products.length === 0) {
+    content = <p>Product List is Empty ⚫</p>;
+  }
+
+  if (!loading && !error && products.length) {
+    content = products
+      .filter((product) => product.rating >= 4)
+      .map((product) => <ProductCard key={product.id} product={product} />);
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      <h1>This is featured page</h1>
+      {content}
     </div>
   );
 };
