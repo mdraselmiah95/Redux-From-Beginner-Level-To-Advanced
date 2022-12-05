850c22079152367a2465addbcb9c3ea0fef40eeb
mdraselmiah95 <russelroy15@gmail.com>
2022-12-05 22:56:41 +0600

    Learning Redux

---
+++ Getting-Started-With-React-Redux/moon-tech-redux/src/components/ProductCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { BiListPlus } from "react-icons/bi";
+import { useProducts } from "../context/ProductProvider";
+import { actionTypes } from "../state/ProductState/actionTypes";
+
+const ProductCard = ({ product }) => {
+  const { dispatch } = useProducts();
+  return (
+    <div
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
+      key={product.id}
+    >
+      <div className="h-52 w-52 mx-auto">
+        <img src={product.image} alt={product.model} />
+      </div>
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
+          {product.keyFeature.map((feature, index) => {
+            return (
+              <li key={index} className="text-sm ">
+                {feature}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="flex gap-2 mt-5">
+        <button
+          onClick={() =>
+            dispatch({ type: actionTypes.ADD_TO_CART, payload: product })
+          }
+          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+        >
+          Add to cart
+        </button>
+        <button
+          title="Add to wishlist"
+          className="bg-indigo-500  py-1 px-2 rounded-full"
+        >
+          <BiListPlus className="text-white" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- Getting-Started-With-React-Redux/moon-tech-redux/src/pages/Home.js
+++ Getting-Started-With-React-Redux/moon-tech-redux/src/pages/Home.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [products, serProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+    <div className="grid grid-cols-1 mx-auto my-10 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14">
       <h1>This is home page</h1>
     </div>
   );
