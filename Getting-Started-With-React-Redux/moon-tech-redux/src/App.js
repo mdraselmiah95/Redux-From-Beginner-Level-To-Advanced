3ce7753b75101f4df0ce73f04d9f4f4175f26391
mdraselmiah95 <russelroy15@gmail.com>
2022-12-05 23:27:13 +0600

    Learning Redux

--- Getting-Started-With-React-Redux/moon-tech-redux/src/App.js
+++ Getting-Started-With-React-Redux/moon-tech-redux/src/App.js
@@ -1,14 +1,17 @@
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./App.css";
 import routes from "./routes/routes";
+import store from "./redux/store";
 
 function App() {
   return (
     <div>
-      <RouterProvider router={routes} />
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
     </div>
   );
 }
 
 export default App;
-// top of the all top
