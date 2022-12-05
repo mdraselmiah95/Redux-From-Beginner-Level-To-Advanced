a0c4f5b7584058b197a05a9ef582ba935da7fb1e
mdraselmiah95 <russelroy15@gmail.com>
2022-12-05 23:31:53 +0600

    Learning Redux

--- Getting-Started-With-React-Redux/moon-tech-redux/src/pages/Home.js
+++ Getting-Started-With-React-Redux/moon-tech-redux/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
@@ -9,6 +10,10 @@ const Home = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const state = useSelector((state) => state);
+
+  console.log(state);
 
   return (
     <div className="grid grid-cols-1 mx-auto my-10 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14">
