c3e4442f5cd3de4e3e5ce1b69e1e575f8b2c81c9
mdraselmiah95 <russelroy15@gmail.com>
2022-12-06 02:26:50 +0600

    Learning React Redux

--- Getting-Started-With-React-Redux/moon-tech-redux/src/components/ProductCard.js
+++ Getting-Started-With-React-Redux/moon-tech-redux/src/components/ProductCard.js
@@ -6,9 +6,11 @@ import {
   addToCart,
   removeFromCart,
 } from "../redux/actionCreators/productActions";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
 
   return (
     <div
@@ -32,24 +34,32 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className="flex gap-2 mt-5">
-        <button
-          onClick={() => dispatch(removeFromCart(product))}
-          className="px-2 py-1 bg-indigo-500 rounded-full"
-        >
-          <RiDeleteBin5Line className="text-white" />
-        </button>
-        <button
-          onClick={() => dispatch(addToCart(product))}
-          className="flex-1 px-2 py-1 text-white bg-indigo-500 rounded-full text-bold"
-        >
-          Add to cart
-        </button>
-        <button
-          title="Add to wishlist"
-          className="px-2 py-1 bg-indigo-500 rounded-full"
-        >
-          <BiListPlus className="text-white" />
-        </button>
+        {pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(removeFromCart(product))}
+            className="flex items-center justify-center flex-1 px-2 py-1 font-semibold text-white bg-red-500 rounded-full "
+          >
+            DELETE
+            <RiDeleteBin5Line className="ml-2 text-white" />
+          </button>
+        )}
+
+        {!pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="flex-1 px-2 py-1 text-white bg-indigo-500 rounded-full text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+        {!pathname.includes("cart") && (
+          <button
+            title="Add to wishlist"
+            className="px-2 py-1 bg-indigo-500 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
       </div>
     </div>
   );
