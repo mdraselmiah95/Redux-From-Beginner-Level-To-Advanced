cb5045de885e37e3dbcc822ea826a6b1640f6c30
mdraselmiah95 <russelroy15@gmail.com>
2022-12-06 15:44:50 +0600

    Learning React Redux

--- Getting-Started-With-React-Redux/moon-tech-redux/src/redux/store.js
+++ Getting-Started-With-React-Redux/moon-tech-redux/src/redux/store.js
@@ -2,6 +2,6 @@ import { createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import productReducer from "./reducers/productReducer";
 
-const store = createStore(productReducer, composeWithDevTools());
+const store = createStore(productReducer, composeWithDevTools()); // =>Using Redux DevToo
 
 export default store;
