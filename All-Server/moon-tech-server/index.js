da0b4080ad00946f11e89c6f940f6b6eabe92393
mdraselmiah95 <russelroy15@gmail.com>
2022-12-06 19:18:18 +0600

    Connecting database

--- All-Server/moon-tech-server/index.js
+++ All-Server/moon-tech-server/index.js
@@ -9,9 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
-const uri =
-  "mongodb+srv://weatherApp:1234567890@cluster0.4kpgg9t.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kpgg9t.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -19,7 +17,6 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-console.log(uri);
 const run = async () => {
   try {
     const db = client.db("moontech");
