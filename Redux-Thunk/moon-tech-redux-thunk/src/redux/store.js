41555d6a8892c5affc8411129c4305f0f078f3db
mdraselmiah95 <russelroy15@gmail.com>
2022-12-07 02:05:51 +0600

    Learning Redux Thunk

--- Redux-Thunk/moon-tech-redux-thunk/src/redux/store.js
+++ Redux-Thunk/moon-tech-redux-thunk/src/redux/store.js
@@ -1,8 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 // import logger from "redux-logger";
 import rootReducer from "./reducers/rootReducer";
+import cartCounter from "./middlewares/cartCounter";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(cartCounter))
+);
 
 export default store;
