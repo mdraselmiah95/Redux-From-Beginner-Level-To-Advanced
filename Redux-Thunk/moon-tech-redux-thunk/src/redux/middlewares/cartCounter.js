09a0105e56f2a4b6c07f2f7862ba14f680bc3f5c
mdraselmiah95 <russelroy15@gmail.com>
2022-12-07 19:37:43 +0600

    Learning Redux Thunk

--- Redux-Thunk/moon-tech-redux-thunk/src/redux/middlewares/cartCounter.js
+++ Redux-Thunk/moon-tech-redux-thunk/src/redux/middlewares/cartCounter.js
@@ -1,8 +1,16 @@
 import { ADD_TO_CART } from "../actionTypes/actionTypes";
 
 const cartCounter = (store) => (next) => (action) => {
+  const state = store.getState();
+  const cart = state.product.cart;
+
   if (action.type === ADD_TO_CART) {
-    return;
+    const newAction = {
+      ...action,
+      payload: { ...action.payload, cartPosition: cart.length },
+    };
+
+    return next(newAction);
   }
 
   return next(action);
