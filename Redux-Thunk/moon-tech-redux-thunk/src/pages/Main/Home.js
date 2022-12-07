076d41f69f71e3253dfae334dd468c13c2fe1213
mdraselmiah95 <russelroy15@gmail.com>
2022-12-07 21:27:44 +0600

    Learning Redux Thunk

--- Redux-Thunk/moon-tech-redux-thunk/src/pages/Main/Home.js
+++ Redux-Thunk/moon-tech-redux-thunk/src/pages/Main/Home.js
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
-import { loadProduct } from "../../redux/actions/productAction";
+import fetchProductData from "../../redux/thunk/products/fetchProducts";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
   const filters = useSelector((state) => state.filter.filters);
+  const products = useSelector((state) => state.product.products);
   const { brands, stock } = filters;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+    dispatch(fetchProductData());
+  }, [dispatch]);
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
