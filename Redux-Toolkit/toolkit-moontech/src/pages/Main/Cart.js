1348cbc24944dbbd4da3fe2c1fea20d932b8daf7
mdraselmiah95 <russelroy15@gmail.com>
2022-12-16 22:28:38 +0600

    Learning Redux toolkit

--- Redux-Toolkit/toolkit-moontech/src/pages/Main/Cart.js
+++ Redux-Toolkit/toolkit-moontech/src/pages/Main/Cart.js
@@ -3,7 +3,7 @@ import React from "react";
 const Cart = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      <h2>This is cart</h2>
+      <h2>This is cart..</h2>
     </div>
   );
 };
