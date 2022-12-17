f7afe39b390b09a4fc9ed620ae60725aaf762b50
mdraselmiah95 <russelroy15@gmail.com>
2022-12-18 00:16:00 +0600

    Learning Redux Toolkit

--- Redux-Toolkit/toolkit-moontech/src/features/products/productsSlice.js
+++ Redux-Toolkit/toolkit-moontech/src/features/products/productsSlice.js
@@ -1,13 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   products: [],
 };
 
+const getProducts = createAsyncThunk("products/getProduct", async () => {
+  const res = await fetch("http://localhost:5000/products");
+  const data = res.json();
+  return data.data;
+});
+
 const productsSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {},
+  extraReducers: {},
 });
 
 export default productsSlice.reducer;
