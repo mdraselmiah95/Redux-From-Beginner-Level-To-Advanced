c82ac440bc2c9a6c292ddde6e9aa26ac1c1567b2
mdraselmiah95 <russelroy15@gmail.com>
2022-12-20 03:15:12 +0600

    Learning Redux Toolkit

--- Redux-Toolkit/toolkit-moontech/src/features/products/productsSlice.js
+++ Redux-Toolkit/toolkit-moontech/src/features/products/productsSlice.js
@@ -4,6 +4,7 @@ import { fetchProducts, postProduct } from "./productAPI";
 const initialState = {
   products: [],
   isLoading: false,
+  postSuccess: false,
   isError: false,
   error: "",
 };
@@ -37,6 +38,22 @@ const productsSlice = createSlice({
       .addCase(getProducts.rejected, (state, action) => {
         state.products = [];
         state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(addProduct.pending, (state) => {
+        state.isLoading = true;
+        state.postSuccess = false;
+        state.isError = false;
+      })
+      .addCase(addProduct.fulfilled, (state) => {
+        state.postSuccess = true;
+        state.isLoading = false;
+      })
+      .addCase(addProduct.rejected, (state, action) => {
+        state.products = [];
+        state.isLoading = false;
+        state.postSuccess = false;
         state.isError = true;
         state.error = action.error.message;
       });
