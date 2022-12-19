25989cbdab3b55d272314b61b69e6970f2f6ba0b
mdraselmiah95 <russelroy15@gmail.com>
2022-12-20 00:54:35 +0600

    Learning Redux Toolkit

--- Redux-Toolkit/toolkit-moontech/src/app/store.js
+++ Redux-Toolkit/toolkit-moontech/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import cartSlice from "../features/cart/cartSlice";
 import filterSlice from "../features/filter/filterSlice";
 import productsSlice from "../features/products/productsSlice";
