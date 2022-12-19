cb7dcd5da6aff28f1536dfdfd58579f14643b1e7
mdraselmiah95 <russelroy15@gmail.com>
2022-12-19 20:15:09 +0600

    new code added

--- Redux-Toolkit/toolkit-moontech/src/features/products/productAPI.js
+++ Redux-Toolkit/toolkit-moontech/src/features/products/productAPI.js
@@ -1,1 +1,32 @@
 // useDispatch hook
+const axios = require("axios");
+
+axios
+  .get("/user?ID=12345")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+axios
+  .get("/user", {
+    params: {
+      ID: 12345,
+    },
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
