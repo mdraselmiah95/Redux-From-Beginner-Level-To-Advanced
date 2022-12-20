fa7a92bf3d16d0af9f5f6e58c89854a71e6a68c5
mdraselmiah95 <russelroy15@gmail.com>
2022-12-20 20:45:57 +0600

    learning Redux Toolkit

--- Redux-Toolkit/toolkit-moontech/src/pages/Dashboard/AddProduct.js
+++ Redux-Toolkit/toolkit-moontech/src/pages/Dashboard/AddProduct.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addProduct } from "../../features/products/productsSlice";
+import { toast } from "react-hot-toast";
 
 const AddProduct = () => {
   const { register, handleSubmit } = useForm();
+  const { isLoading, postSuccess, error, isError } = useSelector(
+    (state) => state.products
+  );
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLoading) {
+      toast.loading("Posting..", { id: "addProduct" });
+    }
+
+    if (!isLoading && postSuccess) {
+      toast.success("Product Added", { id: "addProduct" });
+    }
+
+    if (!isLoading && isError) {
+      toast.error(error, { id: "addProduct" });
+    }
+  }, [isLoading, postSuccess, isError, error]);
 
   const submit = (data) => {
     const product = {
