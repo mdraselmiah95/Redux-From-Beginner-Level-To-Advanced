ea486b8a7e4f7dbb5234ab16fbc72c419f4bfe7b
mdraselmiah95 <russelroy15@gmail.com>
2023-01-01 02:32:46 +0600

    WIP on Redux toolkit final project

--- Final-project/job-box/src/app/store.js
+++ Final-project/job-box/src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "../features/auth/authSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSlice,
+  },
 });
 
 export default store;
