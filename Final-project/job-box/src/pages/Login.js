0539f49b42fc8dacba9ee7571d89bf2135d4838d
mdraselmiah95 <russelroy15@gmail.com>
2023-01-01 02:18:08 +0600

    WIP on Redux toolkit final project

--- Final-project/job-box/src/pages/Login.js
+++ Final-project/job-box/src/pages/Login.js
@@ -10,37 +10,36 @@ const Login = () => {
   const onSubmit = (data) => {
     console.log(data);
   };
-
   return (
-    <div className='flex h-screen items-center'>
-      <div className='w-1/2'>
-        <img src={loginImage} className='h-full w-full' alt='' />
+    <div className="flex items-center h-screen">
+      <div className="w-1/2">
+        <img src={loginImage} className="w-full h-full" alt="" />
       </div>
-      <div className='w-1/2 grid place-items-center'>
-        <div className='bg-[#FFFAF4] rounded-lg grid place-items-center p-10'>
-          <h1 className='mb-10 font-medium text-2xl'>Login</h1>
+      <div className="grid w-1/2 place-items-center">
+        <div className="bg-[#FFFAF4] rounded-lg grid place-items-center p-10">
+          <h1 className="mb-10 text-2xl font-medium">Login</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='space-y-3'>
-              <div className='flex flex-col items-start'>
-                <label htmlFor='email' className='ml-5'>
+            <div className="space-y-3">
+              <div className="flex flex-col items-start">
+                <label htmlFor="email" className="ml-5">
                   Email
                 </label>
-                <input type='email' {...register("email")} id='email' />
+                <input type="email" {...register("email")} id="email" />
               </div>
-              <div className='flex flex-col items-start'>
-                <label htmlFor='password' className='ml-5'>
+              <div className="flex flex-col items-start">
+                <label htmlFor="password" className="ml-5">
                   Password
                 </label>
                 <input
-                  type='password'
-                  id='password'
+                  type="password"
+                  id="password"
                   {...register("password")}
                 />
               </div>
-              <div className='relative !mt-8'>
+              <div className="relative !mt-8">
                 <button
-                  type='submit'
-                  className='font-bold text-white py-3 rounded-full bg-primary w-full'
+                  type="submit"
+                  className="w-full py-3 font-bold text-white rounded-full bg-primary"
                 >
                   Login
                 </button>
@@ -49,7 +48,7 @@ const Login = () => {
                 <p>
                   Don't have an account?{" "}
                   <span
-                    className='text-primary hover:underline cursor-pointer'
+                    className="cursor-pointer text-primary hover:underline"
                     onClick={() => navigate("/signup")}
                   >
                     Sign up
