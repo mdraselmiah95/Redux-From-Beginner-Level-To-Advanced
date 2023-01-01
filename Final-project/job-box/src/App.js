e6a4c6b1daf8110f4fc52a20aea804e76df044a5
mdraselmiah95 <russelroy15@gmail.com>
2023-01-02 01:56:06 +0600

    WIP on Job Box login

--- Final-project/job-box/src/App.js
+++ Final-project/job-box/src/App.js
@@ -2,13 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import auth from "./firebase/firebase.config";
 import { setUser } from "./features/auth/authSlice";
 
 function App() {
   // console.log(process.env)
+  const { isLoading } = useSelector((state) => state.auth);
+  console.log(isLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
