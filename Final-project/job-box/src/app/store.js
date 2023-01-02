5c40777810ba0313b77215b83daa95670cb0ece3
mdraselmiah95 <russelroy15@gmail.com>
2023-01-02 23:47:37 +0600

    WIP on Job Box Server setup

--- Final-project/job-box/src/app/store.js
+++ Final-project/job-box/src/app/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../features/auth/authSlice";
+import apiSlice from "../features/api/apiSlice";
 
 const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
