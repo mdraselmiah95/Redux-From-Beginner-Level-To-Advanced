070deb90c3b78ac786ac6fd6c7170619ddde0ee1
mdraselmiah95 <russelroy15@gmail.com>
2023-01-04 00:50:19 +0600

    Learning Redux by crash course

--- Reduxjs-Crash-Course/reduxjs-crash/src/App.js
+++ Reduxjs-Crash-Course/reduxjs-crash/src/App.js
@@ -1,30 +1,6 @@
 import "./App.css";
-import { createStore } from "redux";
 
 function App() {
-  /**
-   * A reducer function must have two parameters
-   * State ,Action
-   */
-
-  const reducer = (state = {}, action) => {
-    if (action.type === "A") {
-      return {
-        A: "I am A",
-      };
-    }
-    return state;
-  };
-
-  const store = createStore(reducer);
-
-  store.subscribe(() => {
-    console.log(store.getState());
-  });
-
-  store.dispatch({ type: "A" });
-  store.dispatch({ type: "some" });
-
   return (
     <div>
       <h2>This is App.</h2>
@@ -34,4 +10,4 @@ function App() {
 
 export default App;
 
-//27:46
+//47:46 ai
---
+++ Reduxjs-Crash-Course/reduxjs-crash/src/components/store/reducers/rootReducers.js
@@ -0,0 +1,9 @@
+const initialState = {
+  count: 0,
+};
+
+function rootReducer(state = initialState, action) {
+  return state;
+}
+
+export default rootReducer;
