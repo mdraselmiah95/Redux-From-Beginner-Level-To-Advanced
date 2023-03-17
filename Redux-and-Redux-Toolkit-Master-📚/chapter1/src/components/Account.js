74b4cf3fac411120ce6ddc315a44d183af58a861
mdraselmiah95 <russelroy15@gmail.com>
2023-03-17 22:07:24 +0600

    React Redux

--- Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Account.js
+++ Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Account.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Account = () => {
+  const [account, setAccount] = useState({ amount: 0 });
+  const [value, setValue] = useState(0);
+
+  const increment = () => {
+    setAccount({ amount: account.amount + 1 });
+  };
+
+  const decrement = () => {
+    setAccount({ amount: account.amount - 1 });
+  };
+  const incrementByAmount = (value) => {
+    setAccount({ amount: account.amount + value });
+  };
+
   return (
     <div className="card">
       <div className="container">
         <h4>
           <b>Account Component</b>
         </h4>
-        <h3>Amount:$</h3>
+        <h3>Amount:$ {account.amount}</h3>
         <h3>points:$</h3>
-        <button>Increment +</button>
-        <button>Decrement -</button>
-        <input></input>
-        <button>Increment By +</button>
+        <button onClick={increment}>Increment +</button>
+        <button onClick={decrement}>Decrement -</button>
+        <input type="text" onChange={(e) => setValue(+e.target.value)}></input>
+        <button onClick={() => incrementByAmount(value)}>Increment By +</button>
         <button>Init Account</button>
       </div>
     </div>
--- Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Bonus.js
+++ Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Bonus.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Bonus = () => {
+  const [points, setPoints] = useState({ value: 0 });
+
+  const increment = () => {
+    setPoints({ value: points.value + 1 });
+  };
   return (
     <div className="card">
       <div className="container">
         <h4>
           <b>Bonus Component</b>
         </h4>
-        <h3>Amount : $</h3>
+        <h3>Amount : ${points.value}</h3>
         <h3>Total point : </h3>
 
-        <button>Increment +</button>
+        <button onClick={increment}>Increment +</button>
       </div>
     </div>
   );
