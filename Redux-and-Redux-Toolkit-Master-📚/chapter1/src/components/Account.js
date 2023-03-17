127437b892caaf56a1144fdccc23ca597db9f902
mdraselmiah95 <russelroy15@gmail.com>
2023-03-17 21:48:56 +0600

    React Redux

---
+++ Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Account.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Account = () => {
+  return (
+    <div className="card">
+      <div className="container">
+        <h4>
+          <b>Account Component</b>
+        </h4>
+        <h3>Amount:$</h3>
+        <h3>points:$</h3>
+        <button>Increment +</button>
+        <button>Decrement -</button>
+        <input></input>
+        <button>Increment By +</button>
+        <button>Init Account</button>
+      </div>
+    </div>
+  );
+};
+
+export default Account;
---
+++ Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Bonus.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Bonus = () => {
+  return (
+    <div className="card">
+      <div className="container">
+        <h4>
+          <b>Bonus Component</b>
+        </h4>
+        <h3>Amount : $</h3>
+        <h3>Total point : </h3>
+
+        <button>Increment +</button>
+      </div>
+    </div>
+  );
+};
+
+export default Bonus;
