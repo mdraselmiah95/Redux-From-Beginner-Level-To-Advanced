fb66bd6a548632a763577ad985aed93432aa47c0
mdraselmiah95 <russelroy15@gmail.com>
2023-03-17 23:16:42 +0600

    React Redux

--- Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Bonus.js
+++ Redux-and-Redux-Toolkit-Master-📚/chapter1/src/components/Bonus.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 const Bonus = () => {
-  const [points, setPoints] = useState({ value: 0 });
+  const [bonus, setBonus] = useState({ points: 0 });
 
   const increment = () => {
-    setPoints({ value: points.value + 1 });
+    setBonus({ points: bonus.points + 1 });
   };
   return (
     <div className="card">
@@ -12,8 +12,8 @@ const Bonus = () => {
         <h4>
           <b>Bonus Component</b>
         </h4>
-        <h3>Amount : ${points.value}</h3>
-        <h3>Total point : </h3>
+        <h3>Amount : $</h3>
+        <h3>Total point :{bonus.points} </h3>
 
         <button onClick={increment}>Increment +</button>
       </div>
