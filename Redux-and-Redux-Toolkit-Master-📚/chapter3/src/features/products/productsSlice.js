3924fbd5804c1ddfb4151e2728579a09b96df0aa
mdraselmiah95 <russelroy15@gmail.com>
2023-03-19 01:21:34 +0600

    RTK query with async thunk playing a project

---
+++ Redux-and-Redux-Toolkit-Master-📚/chapter3/src/features/products/productsSlice.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchProducts } from "./productsAPI";
+
+const initialState = {
+  products: [],
+  status: "idle",
+};
+
+export const fetchAsync = createAsyncThunk(
+  "products/fetchProduct",
+  async () => {
+    const response = await fetchProducts();
+    return response.data;
+  }
+);
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState,
+
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.value += action.payload;
+      });
+  },
+});
+
+export const {} = productsSlice.actions;
+
+export default productsSlice.reducer;
