30f2dc268b250143085afa9b75417f70daa22189
mdraselmiah95 <russelroy15@gmail.com>
2023-03-19 19:32:12 +0600

    Shopping Cart project with Async Thunk

--- Redux-and-Redux-Toolkit-Master-📚/chapter3/src/app/store.js
+++ Redux-and-Redux-Toolkit-Master-📚/chapter3/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsSlice from "../features/products/productsSlice";
+import cartSlice from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
     product: productsSlice,
+    cart: cartSlice,
   },
 });
--- Redux-and-Redux-Toolkit-Master-📚/chapter3/src/features/cart/Cart.js
+++ Redux-and-Redux-Toolkit-Master-📚/chapter3/src/features/cart/Cart.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.items);
+
   return (
     <div>
-      <h2>This is Cart Components.</h2>
+      <div>
+        {items.map((item) => (
+          <div className="cart-item">
+            <img className="img-fluid" src={item.thumbnail} alt="" />
+            <div className="description">
+              <p>{item.title}</p>
+              <span>{item.brand}</span>
+              <strong>${item.price}</strong>
+            </div>
+            <div className="quantity">
+              Quantity
+              <select
+                value={item.quantity}
+                // onChange={(e) => handleChange(e, item.id)}
+              >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+              </select>
+            </div>
+            <div className="close">
+              {/* <button onClick={() => dispatch(deleteAsync(item.id))}>X</button> */}
+            </div>
+          </div>
+        ))}
+      </div>
+      <h1>
+        Total:{items.reduce((acc, item) => item.price * item.quantity + acc, 0)}
+      </h1>
     </div>
   );
 };
